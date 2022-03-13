9d7bdc4754c1f84151a8e6818cd249f21183ec72
completez <96483406+completez@users.noreply.github.com>
2022-03-13 15:28:55 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,23 @@
+
+
+import requests
+import json
+
+
+url = 'https://gift.truemoney.com/campaign/vouchers/gvdfhbfgngnj/redeem'
+
+x = url.split("/")
+
+CODE = str(x[5])
+
+phone = ""
+
+json = {"mobile" : phone, "voucher_hash" : CODE}
+header = {
+    'Accept' : 'application/json',
+    'Content-type' : 'application/json',
+    'Origin' : 'https://gift.truemoney.com',
+    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43'
+}
+response = requests.post(url,json=json,headers=header)
+print(response.json)
